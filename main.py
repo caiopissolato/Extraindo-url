f413b531ae81b7093498a44c484a8ea537a13cd0
Caio Pissolato <caio_pissolato.05@hotmail.com>
2022-02-22 14:56:57 -0300

    Extraindo strings de um texto - Básico

---
+++ main.py
@@ -0,0 +1,10 @@
+url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+print(url)
+
+url_base = url[0:26]
+
+print(url_base)
+
+url_parametros = url[27:77]
+
+print(url_parametros)
