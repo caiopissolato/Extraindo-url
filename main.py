3521042bb139fd77c63327c80703ab7c17b48c44
Caio Pissolato <caio_pissolato.05@hotmail.com>
2022-02-22 15:04:09 -0300

    Buscando uma string específica

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
 
-url_base = url[0:26]
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao] # sem passar o argumento do início ele interpreta que precisa começar do início da string
 
 print(url_base)
 
-url_parametros = url[27:77]
+url_parametros = url[indice_interrogacao+1:] # sem passar o argumento do fim ele interpreta que precisa ir até o fim
 
 print(url_parametros)
